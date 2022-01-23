fc7c7e3d4745b1bc254a38cbc7fc71664b1898e0
ASCorsen <andrew.corsen@gmail.com>
2022-01-23 15:09:22 +0100

    nav done with styling

---
+++ src/components/navigation/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+
+const NavBar = () => {
+    return (
+        <div>
+<span className="logo-container">
+    <img src={logo} alt="logo"/>
+
+</span>
+
+            Hier komt navigation shit
+
+        </div>
+    );
+};
+
+export default NavBar;
